2fc3ef51d664c8649008f75b502560246d5c54ad
CypherPoet <CypherPoet@gmail.com>
2019-12-02 17:33:11 -0600

    Add mesh functionality to polygon shape

--- TrigRecipies/TrigRecipies/Reusables/Shapes/Polygon.swift
+++ TrigRecipies/TrigRecipies/Reusables/Shapes/Polygon.swift
@@ -14,6 +14,7 @@ import CypherPoetMathUtils_Trig
 struct Polygon {
     var sideCount: Int
     var scale: CGFloat
+    var drawAsMesh: Bool
     
     /// The number of sides represented with its decimal part
     /// so we can animate any changes between whole-numbers of sides.
@@ -22,10 +23,12 @@ struct Polygon {
     
     init(
         sideCount: Int,
-        scale: CGFloat = 1
+        scale: CGFloat = 1,
+        drawAsMesh: Bool = false
     ) {
         self.sideCount = sideCount
         self.scale = scale
+        self.drawAsMesh = drawAsMesh
         
         self.sideCountAsFloat = CGFloat(sideCount)
     }
@@ -57,22 +60,29 @@ extension Polygon: Shape {
         let center = self.center(in: rect)
         let hypotenuse = self.hypotenuse(in: rect)
         
+        var vertices: [CGPoint] = []
         var path = Path()
         
         for index in (0 ..< sidesToDraw) {
             let angle = Double(index) * ((2 * .pi) / Double(sideCountAsFloat))
-            let vertexPosition = point(from: center, atDistance: hypotenuse, inDirection: .radians(angle))
+            let vertex = point(from: center, atDistance: hypotenuse, inDirection: .radians(angle))
             
             if index == 0 {
                 // move to the first vertex
-                path.move(to: vertexPosition)
+                path.move(to: vertex)
             } else {
                 // draw a line to the next vertex
-                path.addLine(to: vertexPosition)
+                path.addLine(to: vertex)
             }
+            
+            if drawAsMesh { vertices.append(vertex) }
         }
         
         path.closeSubpath()
+        
+        if drawAsMesh {
+            drawMeshLines(path: &path, vertices: vertices)
+        }
 
         return path
     }
@@ -100,4 +110,27 @@ extension Polygon {
 
 // MARK: - Private Helpers
 extension Polygon {
+    
+    private func drawMeshLines(
+        path: inout Path,
+        vertices: [CGPoint],
+        startingVertexIndex: Int = 0
+    ) {
+        let vertexCount = vertices.count
+        
+        // We need to have a vertex other than the starting point and its
+        // adjacent vertex (which is already drawn as the perimeter)
+        guard vertexCount - startingVertexIndex > 2 else { return }
+
+        let startingVertex = vertices[startingVertexIndex]
+        
+        for vertexOffset in 2 ..< vertexCount {
+            let targetVertex = vertices[(startingVertexIndex + vertexOffset) % vertexCount]
+            
+            path.move(to: startingVertex)
+            path.addLine(to: targetVertex)
+        }
+        
+        drawMeshLines(path: &path, vertices: vertices, startingVertexIndex: startingVertexIndex + 1)
+    }
 }
--- TrigRecipies/TrigRecipies/Scenes/CustomPolygonExample.swift
+++ TrigRecipies/TrigRecipies/Scenes/CustomPolygonExample.swift
@@ -12,6 +12,7 @@ import SwiftUI
 struct CustomPolygonExample: View {
     @State private var sideCount: CGFloat = 3.0
     @State private var scale: CGFloat = 1.0
+    @State private var drawAsMesh: Bool = false
 }
 
 
@@ -22,10 +23,11 @@ extension CustomPolygonExample {
         VStack {
             Polygon(
                 sideCount: Int(sideCount),
-                scale: scale
+                scale: scale,
+                drawAsMesh: drawAsMesh
             )
+            .stroke(Color.red)
             .animation(.easeInOut(duration: 1.3))
-            
             
             controls
                 .padding()
@@ -44,23 +46,39 @@ extension CustomPolygonExample {
 extension CustomPolygonExample {
     
     private var controls: some View {
-        VStack(spacing: 22.0) {
+//        VStack(spacing: 22.0) {
+        List {
             
-            VStack(spacing: 8) {
+            HStack {
+                Text("Draw As Mesh")
+
+                Spacer()
+                
+                Toggle(isOn: $drawAsMesh) {
+                    Text("Draw As Mesh")
+                }
+            }
+            .padding(.vertical)
+            
+            
+            VStack(spacing: 2) {
                 Text("Sides: \(sideCount)")
 
                 Slider(value: $sideCount, in: 1...30, step: 1, minimumValueLabel: Text("1"), maximumValueLabel: Text("30")) {
                     Text("Side Count")
                 }
             }
+            .padding(.vertical)
             
-            VStack(spacing: 8) {
+            
+            VStack(spacing: 2) {
                 Text("Scale: \(scale)")
-
+                
                 Slider(value: $scale, in: 0.01...1, minimumValueLabel: Text("0.01"), maximumValueLabel: Text("1")) {
                     Text("Scale: \(scale)")
                 }
             }
+            .padding(.vertical)
         }
         .labelsHidden()
     }
